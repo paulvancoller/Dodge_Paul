ae9925f900b71df08b4f30a7d9921364f89f5095
paulvancoller <paul@vancoller.com>
2014-05-01 23:17:01 +0200

    Init Commit + Game Loop

---
+++ Dodge_Paul/Dodge_Paul/Classes/Keyboard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodge_Paul.Classes
+{
+    internal enum KeyCode : int
+    {
+        Left = 0x25,
+        Right = 0x27, 
+        Escape = 0x1B
+    }
+
+    internal static class Keyboard
+    {
+        private const int KeyPressed = 0x8000;
+
+        public static bool IsKeyDown(KeyCode key)
+        {
+            return (GetKeyState((int)key) & KeyPressed) != 0;
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern short GetKeyState(int key);
+    }
+}
---
+++ Dodge_Paul/Dodge_Paul/Program.cs
@@ -0,0 +1,44 @@
+﻿using Dodge_Paul.Classes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodge_Paul
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initialise Game Objects
+            bool Quit = false;
+            Form GameForm = new Form() { WindowState = FormWindowState.Maximized, FormBorderStyle = System.Windows.Forms.FormBorderStyle.None, BackColor = Color.Black, Text = "Dodge", MaximizeBox = false, MinimizeBox = false, ControlBox = false };
+            GameForm.Show();
+
+            //Game loop
+            while (!Quit)
+            {
+
+                Thread.Sleep(20);
+                if (Keyboard.IsKeyDown(KeyCode.Escape))
+                    Quit = true;
+
+                Application.DoEvents();
+            }
+
+
+            // Cleanup game objects
+            GameForm = null;
+        }
+    }
+}
